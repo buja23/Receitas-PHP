bc2091bbf7e395300029cef979a0f21be34c9f29
buja23 <jooj.soares.227@gmail.com>
2025-11-14 16:00:00 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Receitas</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Receitas</h1>
+
+        <form action="index.php" method="get" class="form-search">
+            <input type="text" name="busca" placeholder="Buscar receita">
+            <button type="submit">Buscar</button>
+        </form>
+
+
+
+
+</body>
+</html>
