1c7c5336bae0fe872068065b09591488e014a077
buja23 <jooj.soares.227@gmail.com>
2025-11-14 18:50:53 -0300

    Mostrando Elementos JSON

--- index.php
+++ index.php
@@ -15,6 +15,38 @@
             <button type="submit">Buscar</button>
         </form>
 
+    <div class="receitas">
+        <?php
+        $json_data = file_get_contents('receitas.json');
+        $receitas = json_decode($json_data, true);
+
+        $busca = isset($_GET['busca']) ? strtolower(trim($_GET['busca'])) : '';
+
+        if ($receitas){
+            foreach ($receitas as $receita){
+                if ($busca === '' || strpos(strtolower($receita['nome']), $busca) !== false){
+                    //exibir receitas
+
+                    //delahe.php no fu
+                    echo '<a href="detalhe.php?id=' . $receita['id'] . '" class="receitas">';
+                    echo '<img src="' . htmlspecialchars($receita['thumbnail']) . '" alt="' . htmlspecialchars($receita['titulo']) . '">';
+                    echo '<h2>' . htmlspecialchars($receita['titulo']) . '</h2>';
+                    echo '<div class="tags">';
+                    foreach ($receita['tags'] as $tag) {
+                        echo '<span>' . htmlspecialchars(string: $tag) . '</span>';
+                    }
+                    echo '</div>';
+                    echo '<p class="tempo-preparo">' . htmlspecialchars($receita['tempo']) . '</p>';
+                    echo '</a>';
+                }
+            }
+
+        }
+
+
+
+
+        ?>
 
 
 
